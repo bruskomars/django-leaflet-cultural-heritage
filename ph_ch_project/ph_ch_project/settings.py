d150c56d3201a797ad43b84d9da79b619150e020
Mars <pythonista.mars@gmail.com>
2026-08-29 14:53:59 +0800

    set up postgis database

--- ph_ch_project/ph_ch_project/settings.py
+++ ph_ch_project/ph_ch_project/settings.py
@@ -94,8 +94,12 @@ WSGI_APPLICATION = 'ph_ch_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME':'ph_ch_project',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
