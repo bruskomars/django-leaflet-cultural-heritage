0e3bd088eae85ef08f95b10bde5e2f2adc14b0aa
Mars <pythonista.mars@gmail.com>
2026-08-29 12:33:16 +0800

    create django app > py manage.py startapp <NAME>

---
+++ ph_ch_project/ch_backend/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ChBackendConfig(AppConfig):
+    name = 'ch_backend'
---
+++ ph_ch_project/ch_frontend/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ChFrontendConfig(AppConfig):
+    name = 'ch_frontend'
