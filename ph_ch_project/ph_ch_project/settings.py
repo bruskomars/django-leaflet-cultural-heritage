bd75d7477efe2248b3de6ea96bf16b026c27be0c
Mars <pythonista.mars@gmail.com>
2026-08-30 13:33:56 +0800

    set up media folder for images

--- ph_ch_project/ph_ch_project/settings.py
+++ ph_ch_project/ph_ch_project/settings.py
@@ -144,6 +144,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
