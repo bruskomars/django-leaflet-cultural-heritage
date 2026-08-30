38c65ff1b86d6eae1f0e99973e75d842d7db1477
Mars <pythonista.mars@gmail.com>
2026-08-30 15:31:51 +0800

    use serialization and database query to views and show the result as geojson response

--- ph_ch_project/ch_backend/urls.py
+++ ph_ch_project/ch_backend/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello_world, name='hello_world'),
+    path('places/', views.all_places, name='all_places'),
+    path('places/<int:pk>', views.place_detail, name='place_detail'),
 ] 
--- ph_ch_project/ch_backend/views.py
+++ ph_ch_project/ch_backend/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.serializers import serialize
+from .models import Place
 
 # Create your views here.
-def hello_world(request):
-    return HttpResponse('Hello world')
+def all_places(request):
+    queryset = Place.objects.all()
+    geojson = serialize('geojson', 
+                        queryset, 
+                        geometry_field='point_geometry', 
+                        srid=4326)
+    
+    return HttpResponse(geojson, content_type='application/json')
+
+def place_detail(request, pk):
+    data = []
+    try:
+        place = Place.objects.get(pk=pk)
+        data.append(place)
+    except Place.DoesNotExist:
+        pass
+
+    geojson = serialize('geojson', data, geometry_field='point_geometry', srid=4326)
+    return HttpResponse(geojson, content_type='application/json')
