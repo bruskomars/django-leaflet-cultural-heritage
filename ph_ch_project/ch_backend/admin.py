2bbf7abbc4ee64b5360b54b36af4fe0305b35935
Mars <pythonista.mars@gmail.com>
2026-08-30 13:58:10 +0800

    register models and create custom geoAdmin for models using geofields

--- ph_ch_project/ch_backend/admin.py
+++ ph_ch_project/ch_backend/admin.py
@@ -1,3 +1,23 @@
-from django.contrib import admin
+from django.contrib.gis import admin
+from .models import Category, Place, City
 
 # Register your models here.
+admin.site.register(Category)
+
+class CustomGeoAdmin(admin.GISModelAdmin):
+    gis_widget_kwargs = {
+        'att': {
+            'default_zoom': 11,
+            'default_lon': 121.037281,
+            'default_lat': 14.569973,
+        }
+    }
+
+
+@admin.register(Place)
+class PlaceAdmin(CustomGeoAdmin):
+    pass
+
+@admin.register(City)
+class CityAdmin(CustomGeoAdmin):
+    pass
