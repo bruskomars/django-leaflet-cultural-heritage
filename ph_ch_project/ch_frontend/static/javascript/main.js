03d084c192088f9096719ca721862e0fa2b9bf8b
Mars Alexis Regidor <pythonista.mars@gmail.com>
2026-09-07 15:25:10 +0800

    Add places features to map view from api url results

--- ph_ch_project/ch_frontend/static/javascript/main.js
+++ ph_ch_project/ch_frontend/static/javascript/main.js
@@ -1,9 +1,42 @@
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
+  // Leaflet map initialization
   const map = L.map("map").setView([14.516589, 121.019333], 13);
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution:
       '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   }).addTo(map);
+
+  //   Fetch the places data from the API - fetch api-get request
+  const fetchGetRequest = async (url, func) => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      return func(data);
+    } catch (error) {
+      console.error("Error fetching data:", error.message);
+    }
+  };
+
+  // Add all places to the map and define the point style for the markers
+  const poinstStyle = {
+    stroke: true,
+    radius: 8,
+    color: "black",
+    weight: 2,
+    opacity: 1,
+    fillColor: "green",
+    fillOpacity: 1,
+  };
+
+  const addAllPlacesToMap = (json) => {
+    let places = L.geoJSON(json, {
+      pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, poinstStyle);
+      },
+    }).addTo(map);
+  };
+
+  fetchGetRequest("/api/v1/places/", addAllPlacesToMap);
 }
