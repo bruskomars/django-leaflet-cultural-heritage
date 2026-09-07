8964f81dee3e25289237b17d38f383a6563b6d37
Mars Alexis Regidor <pythonista.mars@gmail.com>
2026-09-07 14:54:45 +0800

    setting up leaflet

--- ph_ch_project/ch_frontend/static/javascript/main.js
+++ ph_ch_project/ch_frontend/static/javascript/main.js
@@ -1,1 +1,9 @@
+document.addEventListener("DOMContentLoaded", init);
 
+function init() {
+  const map = L.map("map").setView([14.516589, 121.019333], 13);
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+}
